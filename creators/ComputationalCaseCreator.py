1556641b61be2fc6cca58626c5f123b00af7d979
Elaine Hale <elaine.hale@nrel.gov>
2014-02-27 11:44:30 -0700

    Working on APIs and on my python coding.

--- creators/ComputationalCaseCreator.py
+++ creators/ComputationalCaseCreator.py
@@ -2,6 +2,27 @@ import Params from CreatorBase
 import Creator from CreatorBase
 
 class ComputationalCaseParams(Params): pass
+    def __init__(self): pass
+        # base_feeder - required, convertible to path
+        # sim_duration - defaults to 1h, convertible to datetime.timedelta
+        # sim_timestep - defaults to 1min, convertible to datetime.timedelta
+        # technologies - defaults to baseline (0), list of integers
+
+    def base_feeder(self): pass
+        "Returns the real path to the base feeder."
+  
+    def sim_duration(self): pass
+        "Returns the datetime.timedelta for the simulation duration."
+        
+    def sim_timestep(self): pass
+        "Returns the datetime.timedelta for the simulation timestep."
+        
+    def technologies(self): pass
+        "HERE -- one or list of technology packages to apply."
+        
+    # def io_intensity(): pass
+    
+    
 
 class ComputationalCaseCreator(Creator): pass
 
--- creators/CreatorBase.py
+++ creators/CreatorBase.py
@@ -1,3 +1,69 @@
+from abc import abstractmethod
+
 class Params: pass
+    """
+    Base class for dictionary-based params with json serialization.
+    """
+    # save to json
+    # save json template
+    # load from json
+    # parameter documentation
+    #   - description  
+    #   - required or not
+    #   - default value
+    #   - parsing method
+    # valid?
+    
+    __schema = {}
+    """
+    @ivar: Dict of parameter descriptors of the form 
+           {param_name = [description, required, default value, parsing method]}.
+    """
+    
+    def __init__(self, values = {}): pass
+        """
+        Initialize a Params object with values.
+        
+        @type values: dict of param_name, param_value
+        @param values: User's parameter values.
+        """
+        
+        # loop through values and keep the ones that fit the schema
+        self.__values = {}
+        
+    def __str__(self): pass
+  
+    def get(self, param_name): pass
+  
+    def set(self, param_name, param_value): pass
+    
+    def clear(self, param_name): pass
+    
+    def __getattr__(self, param_name): 
+        return self.get(param_name)
+        
+    def __setattr__(self, param_name, param_value):
+        self.set(param_name,param_value)
+        
+    def __delattr__(self, param_name):
+        self.clear(param_name)
+  
+    def to_json(self): pass
+  
+    def save(self, path): pass
+  
+    def json_template(self): pass
+  
+    def save_template(self, path): pass
+  
+    @abstractmethod
+    def load(self, path):
+  
+    def valid(self): pass
+    
+    def __nonzero__(self): 
+        return self.valid()
+  
+  
 
 class Creator: pass
