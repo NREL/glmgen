a7045699717fd7b6f15bbe1ca22f671952adb2ce
Elaine Hale <elaine.hale@nrel.gov>
2014-05-27 15:56:23 -0600

    Trying to get pip install from git to work.

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from distutils.core import setup
 setup(
     name='glmgen',
     version='0.1.0',
-    author='TBD',
-    author_email='TBD',
+    author='Elaine Hale, forked from https://github.com/elainethale/omf',
+    author_email='elaine.hale@nrel.gov',
     packages=['glmgen', 'glmgen.test', 'glmgen.schedules'],
     package_data={'': ['*.glm', '*.player', '*.csv']},
-    scripts=[],
-    url='TBD',
-    license='LICENSE.txt',
+    url='https://github.nrel.gov/ESI/omf-glm-generator',
     description='Utilities for manipulating GridLAB-D model (glm) files in Python.',
-    long_description=open('README.txt').read(),
     install_requires=open('requirements.txt').read(),
 )
