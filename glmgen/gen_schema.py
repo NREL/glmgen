e430d152b29145d5f1b0b808c18d70fc163b10c8
Elaine Hale <elaine.hale@nrel.gov>
2014-07-03 15:06:41 -0600

    Sketch of how to auto-generate gridlabd schema.

---
+++ glmgen/gen_schema.py
@@ -0,0 +1,22 @@
+
+def main(gld_install_path):
+
+    # get gld version
+
+    # get list of candidate module names
+    candidate_modules = [] # should be set, not list
+    # file names in lib
+        
+    # module XXXX calls in samples .glms
+    
+
+    # populate schema by scraping modhelp
+    for candidate in candidate_modules:
+        "gridlabd --modhelp {}".format(candidate)
+        
+if __name__ == "__main__":
+    # get gld_install_path and out_dir
+    
+    main(gld_install_path)
+        
+    
